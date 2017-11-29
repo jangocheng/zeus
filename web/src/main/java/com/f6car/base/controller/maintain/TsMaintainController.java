5c454ac8c9c3210b391819cf682b756f0f4c7175
qixiaobo <qixiaobo.nj@gmail.com>
2017-11-30 01:01:56 +0800

    mybatis多数据源

---
+++ web/src/main/java/com/f6car/base/controller/maintain/TsMaintainController.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.f6car.base.controller.maintain;
+
+import com.f6car.base.controller.base.AbstractRestController;
+import com.f6car.base.service.maintain.TsMaintainService;
+import com.f6car.base.so.maintain.TsMaintainSo;
+import com.f6car.base.vo.maintain.TsMaintainVo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.math.BigInteger;
+
+
+/**
+ * Created by qixiaobo on 2017-11-29.
+ */
+@RestController
+@RequestMapping("maintain")
+public class TsMaintainController extends AbstractRestController<TsMaintainVo, TsMaintainSo, BigInteger> {
+    @Resource
+    private TsMaintainService tsMaintainService;
+
+
+}
