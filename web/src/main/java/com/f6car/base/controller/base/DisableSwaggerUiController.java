55a3b5db0a822773fee0c059b4fe2905abe37eee
qixiaobo <qixiaobo.nj@gmail.com>
2017-12-08 10:06:09 +0800

    product环境不展示

---
+++ web/src/main/java/com/f6car/base/controller/base/DisableSwaggerUiController.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.f6car.base.controller.base;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author qixiaobo
+ */
+@Profile("prod")
+@RestController
+public class DisableSwaggerUiController {
+    @RequestMapping(value = "swagger-ui.html", method = RequestMethod.GET)
+    public void getSwagger(HttpServletResponse httpResponse) throws IOException {
+        httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+    }
+}
