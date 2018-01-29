0a297989f660386014985855178e5ea3a0e60042
qixiaobo <qixiaobo.nj@gmail.com>
2018-01-29 15:44:00 +0800

    :alien: Updating code due to external API changes. 生成文档

--- core/src/main/java/com/f6car/base/core/DateUtil.java
+++ core/src/main/java/com/f6car/base/core/DateUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
  * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
@@ -21,32 +21,32 @@ public final class DateUtil {
     private DateUtil() {
     }
 
-    public String getDateFormat(Date date) {
+    public static String getDateFormat(Date date) {
         return getDateFormat(date, Constants.DEFAULT_DATE_FORMAT_PATTERN);
     }
 
-    public String getDateFormat(Date date, String format) {
+    public static String getDateFormat(Date date, String format) {
         return new DateTime(date).toString(format);
 
     }
 
-    public Date parseDate(String dateStr) {
+    public static Date parseDate(String dateStr) {
         return parseDate(dateStr, Constants.DEFAULT_DATE_FORMAT_PATTERN);
     }
 
-    public Date parseDate(String dateStr, String format) {
+    public static Date parseDate(String dateStr, String format) {
         return DateTimeFormat.forPattern(format).parseDateTime(format).toDate();
     }
 
-    public Date addDay(Date date, int day) {
+    public static Date addDay(Date date, int day) {
         return new DateTime(date).plusDays(day).toDate();
     }
 
-    public Date addMonth(Date date, int month) {
+    public static Date addMonth(Date date, int month) {
         return new DateTime(date).plusMonths(month).toDate();
     }
 
-    public Date addHour(Date date, int hour) {
+    public static Date addHour(Date date, int hour) {
         return new DateTime(date).plusHours(hour).toDate();
     }
 
