241740248bbed53c1f9f98223149aa1a1498d232
qixiaobo <qixiaobo.nj@gmail.com>
2018-08-02 15:31:22 +0800

    :ok_hand: Updating code due to code review changes. 离线文档生成

---
+++ web/src/test/java/com/f6car/base/test/SwaggerStaticDocTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.f6car.base.test;
+
+import io.github.robwin.markup.builder.MarkupLanguage;
+import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import springfox.documentation.staticdocs.SwaggerResultHandler;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerStaticDocTest {
+    @Autowired
+    private WebApplicationContext context;
+    private String snippetDir = "target/generated-snippets";
+    private String outputDir = "target/asciidoc";
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void Test() throws Exception {
+        // 得到swagger.json,写入outputDir目录中
+        mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andDo(SwaggerResultHandler.outputDirectory(outputDir).build())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // 读取上一步生成的swagger.json转成asciiDoc,写入到outputDir
+        // 这个outputDir必须和插件里面<generated></generated>标签配置一致
+        Swagger2MarkupConverter.from(outputDir + "/swagger.json")
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)// 格式
+                .withExamples(snippetDir)
+                .build()
+                .intoFolder(outputDir);// 输出
+    }
+
+
+}
