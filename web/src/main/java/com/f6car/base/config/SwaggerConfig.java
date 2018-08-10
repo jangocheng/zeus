e6b4a705e5cbc7fea2cb4028daf45974ce39fc29
qixiaobo <qixiaobo.nj@gmail.com>
2018-08-10 16:42:15 +0800

    :ok_hand: Updating code due to code review changes. 升级swagger get方法增加modelschema 同时根据jwt提供授权头

--- web/src/main/java/com/f6car/base/config/SwaggerConfig.java
+++ web/src/main/java/com/f6car/base/config/SwaggerConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
  * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
@@ -9,17 +9,26 @@
 package com.f6car.base.config;
 
 import com.f6car.base.constant.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.f6car.base.constant.Constants.AUTHENTICATION;
 
 /**
  * @author qixiaobo
@@ -31,7 +40,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket createRestApi() {
+    public Docket createRestApi(@Autowired(required = false) List<Parameter> parameterList) {
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("使用Swagger2构建RESTful APIs")
                 .description("客户端与服务端接口文档")
@@ -45,7 +54,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(Constants.CONTROLLER_PACKAGE))
                 .paths(PathSelectors.any())
-                .build();
+                .build().globalOperationParameters(parameterList);
+    }
+
+    @ConditionalOnProperty(name = Constants.PROPERTY_WEB_SECURE, havingValue = Constants.WEB_SECURE_JWT)
+    @Bean
+    public Parameter authHeader() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name(AUTHENTICATION).description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        return tokenPar.build();
     }
 
 }
